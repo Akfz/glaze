7c56cff95801ae7607999aa3d4d94e4b38f77795
akaize <sahsa61842@gmail.com>
2026-09-01 14:14:55 +0300

    rewrt

--- common/src/main/java/v/akfz/glaze/Glaze.java
+++ common/src/main/java/v/akfz/glaze/Glaze.java
@@ -1,15 +1,14 @@
 package v.akfz.glaze;
 
-import net.minecraft.resources.ResourceLocation;
+import v.akfz.aslib.resourcepack.ModAssetsRegistrar;
 import v.akfz.db.generator.GenerateInitializer;
-import v.akfz.glaze.impl.post.PostProcessRenderer;
 
 @GenerateInitializer(modId = "glz")
 public class Glaze {
-	public static boolean postProcess = true;
-	public static boolean addictiveLight = false;
+	public static boolean postProcess = false;
+	//public static boolean addictiveLight = true; if i add this, it will be custom render world pipeline, maybe on vulkan, or i really add this like addictive, idk
 
 	public void init() {
-		PostProcessRenderer.INSTANCE.addShader(new ResourceLocation("glaze", "shader/test/postprocess/pptest.glsl"));
+		ModAssetsRegistrar.registerModAssets("glz");
 	}
 }
